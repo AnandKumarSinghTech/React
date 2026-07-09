8ec12708e6f642d344ed7b4478ed007f6fae150e
Anand <anand@example.com>
2026-07-09 10:12:47 +0530

    Initializing react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username="Anand Singh"
+  return (
+    <>
+   <Chai/>
+    <h3>Hello from react {username}</h3>
+   <p>tesing para</p>
+    </>
+  )
+}
+
+export default App
